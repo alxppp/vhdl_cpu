cd0ac126ab058a1aea2d07d2f1e8ba6f01aad289
OrestisAlpos <or.al@hotmail.com>
2015-11-05 15:40:50 +0100

    Functions erstellt
    Funtions EXEC_REA, EXEC_REO, EXEC_REX

--- work/cpu_subprogram_pack_orestis.vhd
+++ work/cpu_subprogram_pack_orestis.vhd
@@ -1,14 +1,31 @@
 package cpu_subprogram_pack_orestis is
 
 	use WORK.cpu_defs_pack.all;
+	use WORK.bit_vector_natural_pack.all;
 
 	function INC(constant PC :in addr_type) return addr_type;
-	procedure EXEC_ADDC (   constant A,B	: in data_type;
-				variable R	: out data_type;
-				variable Z	: out Boolean;
-				variable C	: inout Boolean;
-				variable N,O	: out Boolean );
 
+	-- ADDC implementation appropriate both for ADD and ADDC instructons. See Skript 152-153.
+	procedure EXEC_ADDC ( constant A,B	: in data_type;
+			      variable R	: out data_type;
+			      variable Z	: out Boolean;
+			      variable C	: inout Boolean;
+			      variable N,O	: out Boolean );
+	
+	--REA: Use AND operator on all bits of Y and assign the result to the LSb of X. All other bits of X are cleared.
+	procedure EXEC_REA ( variable A		: out data_type;
+			     constant B		: in data_type;
+			     variable Z,C,N,O	: out Boolean );
+
+	--REO: Use OR operator on all bits of Y and assign the result to the LSb of X. All other bits of X are cleared.
+	procedure EXEC_REO ( variable A		: out data_type;
+			     constant B		: in data_type;
+			     variable Z,C,N,O	: out Boolean );	
+
+	--REA: Use XOR operator on all bits of Y and assign the result to the LSb of X. All other bits of X are cleared.
+	procedure EXEC_REX ( variable A		: out data_type;
+			     constant B		: in data_type;
+			     variable Z,C,N,O	: out Boolean );
 
 end cpu_subprogram_pack_orestis;
 
@@ -24,7 +41,7 @@ package body cpu_subprogram_pack_orestis is
 	end INC;
 
 
-	-- ADDC implementation appropriate both for ADD and ADDC instructons. See Skript 152-153.
+
 	procedure EXEC_ADDC (   constant A,B	: in data_type;
 				variable R	: out data_type;
 				variable Z	: out Boolean;
@@ -73,5 +90,68 @@ package body cpu_subprogram_pack_orestis is
 			O := FALSE;
 		end if;
 	end EXEC_ADDC;	
+
+
+
+	procedure EXEC_REA (variable A		: out data_type;
+			    constant B		: in data_type;
+			    variable Z,C,N,O	: out Boolean ) is
+	begin
+		if B = 2**data_width - 1 then --result is 1 iff all bits of Y are 1
+			A := 1;
+			Z := FALSE;
+		else
+			A := 0;
+			Z := TRUE;
+		end if;
+		C := FALSE;
+		N := FALSE;
+		O := FALSE;
+	end EXEC_REA;
+
+
+
+	procedure EXEC_REO (variable A		: out data_type;
+			    constant B		: in data_type;
+			    variable Z,C,N,O	: out Boolean ) is
+	begin
+		if B > 0 then --result is 1 if at least one bit in Y is 1
+			A := 1;
+			Z := FALSE;
+		else
+			A := 0;
+			Z := TRUE;
+		end if;
+		C := FALSE;
+		N := FALSE;
+		O := FALSE;
+	end EXEC_REO;
+
+
+
+	procedure EXEC_REX (variable A		: out data_type;
+			    constant B		: in data_type;
+			    variable Z,C,N,O	: out Boolean ) is
+		variable B_bv	: bit_vector(data_width-1 downto 0) := natural2bit_vector(B, data_width);
+		variable result	: bit := B_bv(data_width-1) ;
+	begin
+		for i in data_width-2 downto 0 loop
+			result := result XOR B_bv(i);
+		end loop;
+		if  result = '1' then 
+			A := 1;
+			Z := FALSE;
+		else
+			A := 0;
+			Z := TRUE;
+		end if;
+		C := FALSE;
+		N := FALSE;
+		O := FALSE;
+	end EXEC_REX;
+
+
+
+
 
 end cpu_subprogram_pack_orestis;

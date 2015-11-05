f26d15e52166305c7646dda61e0020b253c8b323
Alexander Popp <alxppp@googlemail.com>
2015-11-05 20:28:05 +0100

    Add CPU pack Alex

---
+++ cpu_subprogram_pack_alex.vhd
@@ -0,0 +1,10 @@
+package cpu_subprogram_pack_alex is
+
+use WORK.cpu_defs_pack.all;
+use WORK.bit_vector_natural_pack.all;
+
+end cpu_subprogram_pack_alex;
+
+package body cpu_subprogram_pack_alex is
+
+end cpu_subprogram_pack_alex;

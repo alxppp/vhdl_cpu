64823e57c6f3a42284a5e937d9844beab5892588
Alexander Popp <alxppp@googlemail.com>
2015-11-05 23:09:59 +0100

    Implement EXEC_SRA

--- cpu_subprogram_pack_alex.vhd
+++ cpu_subprogram_pack_alex.vhd
@@ -6,6 +6,7 @@ package cpu_subprogram_pack_alex is
 
 	procedure EXEC_SLL(constant A : in data_type; variable B : out data_type; variable Z, C, N, O : out Boolean);
 	procedure EXEC_SRL(constant A : in data_type; variable B : out data_type; variable Z, C, N, O : out Boolean);
+	procedure EXEC_SRA(constant A : in data_type; variable B : out data_type; variable Z, C, N, O : out Boolean);
 
 end cpu_subprogram_pack_alex;
 
@@ -40,5 +41,17 @@ package body cpu_subprogram_pack_alex is
 		B := R;
 
 	end EXEC_SRL;
+
+	procedure EXEC_SRA(constant A : in data_type; variable B : out data_type; variable Z, C, N, O : out Boolean) is
+		variable T : bit_vector(data_width - 1 downto 0);
+		variable R : data_type;
+
+	begin
+		C := bit2boolean(T(T'right));
+		T := T(T'right) & T(T'left downto 1);
+		R := bit_vector2natural(T);
+		B := R;
+
+	end EXEC_SRA;
 
 end cpu_subprogram_pack_alex;

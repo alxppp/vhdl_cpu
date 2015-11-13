f2c7276e76306da24fac25d80a99375cb618d614
Alexander Popp <alxppp@googlemail.com>
2015-11-13 20:42:25 +0100

    Add second not test

--- testbenches/not.asm
+++ testbenches/not.asm
@@ -4,12 +4,19 @@ TEST1:	LDC	R1	0		; test ⌐0 == #FFF
 	NOT	R1	R1
 	LDC	R2	#FFF		; expected result if #FFF
 	SUB	R1	R1	R2	; see if result - expected result is 0
-	JZ	SUCCESS			; delta is 0 -> success
+	JZ	TEST2			; delta is 0 -> success
 	JMP	ERROR			; delta is not 0 -> error
 
+TEST2:	LDC	R1	#F0		; test ⌐#F0 == #F0F
+	NOT	R1	R1
+	LDC	R2	#F0F
+	SUB	R1	R1	R2
+	JZ	SUCCESS
+	JMP	ERROR
+
 SUCCESS:
-	LDC	R1	0
+	LDC	R0	0
 	STOP
 
-ERROR:	LDC	R1	1
+ERROR:	LDC	R0	1
 	STOP

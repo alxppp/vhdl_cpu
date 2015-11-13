6c85ea8a79adfb8fb75109b67d90535233d9d425
Alexander Popp <alxppp@googlemail.com>
2015-11-13 20:15:33 +0100

    Update not test

--- testbenches/not.asm
+++ testbenches/not.asm
@@ -1,5 +1,15 @@
 ; Test NOT
 
-	LDC	R1	0	; Test NOT
-	NOT	R2	R1
+TEST1:	LDC	R1	0		; test ⌐0 == #FFF
+	NOT	R1	R1
+	LDC	R2	#FFF		; expected result if #FFF
+	SUB	R1	R1	R2	; see if result - expected result is 0
+	JZ	SUCCESS			; delta is 0 -> success
+	JMP	ERROR			; delta is not 0 -> error
+
+SUCCESS:
+	LDC	R1	0
+	STOP
+
+ERROR:	LDC	R1	1
 	STOP

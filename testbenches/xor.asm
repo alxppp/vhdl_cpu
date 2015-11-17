d8b6146c3ecf947a5f990c7201f5e94cf183b1a9
Alexander Popp <alxppp@googlemail.com>
2015-11-17 23:49:23 +0100

    Refine XOR test

--- testbenches/xor.asm
+++ testbenches/xor.asm
@@ -19,8 +19,14 @@ TEST3:	LDC	R1	1		; test 1 ⊻ 2 == 3
 
 TEST4:	LDC	R1	#FFF		; test #FFF ⊻ #FFF == 0
 	XOR	R1	R1	R1
-	LDC	R2	#FFF
-	SUB	R1	R1	R2
+	JNZ	ERROR
+
+TEST5:	LDC	R1	#ABC		; test XOR swap
+	XOR	R1	R1	R2	; swap R1 and R2
+	XOR	R2	R1	R2	; ------ " -----
+	XOR	R1	R1	R2	; ------ " -----
+	LDC	R3	#ABC		; R2 should be #ABC now
+	SUB	R1	R2	R3
 	JNZ	ERROR
 
 SUCCESS:

55f87c4a096dbae2b0408bf66fc48e75ba904b76
Alexander Popp <alxppp@googlemail.com>
2015-11-17 23:20:42 +0100

    Fix LDPC test and add trace

--- testbenches/ldpc.asm
+++ testbenches/ldpc.asm
@@ -1,10 +1,9 @@
 ; Test LDPC
 
 TEST1:	LDC	R1	TEST1		; R1 = &TEST1
-	LDPC	R2			; R2 = PC, R2 should be R1 + 2
-	LDC	R3	2
-	SUB	R3	R2	R3	; R1 = R2
-	SUB	R1	R1	R2
+	LDPC	R2			; R2 = PC, R2 should be R1 + 3
+	LDC	R3	3
+	SUB	R3	R2	R3
 	JNZ	ERROR
 
 SUCCESS:

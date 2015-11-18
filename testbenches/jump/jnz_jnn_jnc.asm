266453d99d388e56734837c0f72e03fb8ede446b
maxcpu <max.ehm@gmx.de>
2015-11-18 15:00:51 +0100

    new test sll srl sra jnz jnn jnc

--- testbenches/jump/jnz_jnn_jnc.asm
+++ testbenches/jump/jnz_jnn_jnc.asm
@@ -1,14 +1,22 @@
-	LDC	R0	#0
+Test1:	LDC	R0	#0
 	JNZ	Error
+	LDC R0	#1
+	JNZ Test2
+	JMP Error
 
-	LDC	R0	#FFF
+Test2:	LDC	R0	#FFF
 	JNN	Error
+	LDC	R0	#1
+	JNN	Test3
+	JMP 	Error
 
-	LDC	R0	#800
+Test3:	LDC	R0	#800
 	ADD	R0	R0	R0
 	JNC	Error
-
-	STOP
+	LDC	R0	#1
+	ADD	R0	R0	R0
+	JNC	End
+	JMP	Error
 
 Error:	LDC	R3	#FFF	;indicates error in trace
-	STOP
+End:	STOP

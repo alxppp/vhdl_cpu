a05842f7c677d7b822d5f96f4b261397d1f4a4bd
RobMa <robert.markl@tum.de>
2015-11-20 11:12:45 +0100

    Modifed REX testbench so that Zero flag is also verified.

--- testbenches/logic/rex.asm
+++ testbenches/logic/rex.asm
@@ -11,7 +11,9 @@ return1:		LDC		R0 0
 test1:			LDC		R1 #000			;loop through all possible values and compare with logfile
 				
 test1_loop:		REX		R2 R1			;C=0, N=0, O=0
-				JC		error
+				JZ		test1if1		;verify Z flag
+				JMP		test1else1		
+test1endif1:	JC		error
 				JN		error
 				JO		error
 				IN		R3				;load compare value from logfile
@@ -24,6 +26,13 @@ test1_loop:		REX		R2 R1			;C=0, N=0, O=0
 				JMP		test1_loop
 test1_endloop:	JMP		return1
 
+test1if1:		OR		R2 R2 R2		;verify for Z=1 --> check if R2==0
+				JNZ		error			
+				JMP		test1endif1
+
+test1else1:		OR		R2 R2 R2		;verify for Z=0 --> check if R2!=0
+				JZ		error
+				JMP		test1endif1
 
 error:			LDC		R0 1
 				STOP

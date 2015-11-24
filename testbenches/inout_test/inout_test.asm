481f1df7b3fe4640b0a05c1d25920f44fefb150b
maxcpu <max.ehm@gmx.de>
2015-11-24 17:34:35 +0100

    complete test of IN, OUT

--- testbenches/inout_test/inout_test.asm
+++
@@ -1,9 +0,0 @@
-	IN	R0	;R0 = 0, Zero = TRUE	
-	IN	R1	;R1 = 00A
-	IN	R2	;R2 = 800, Negative = TRUE
-	IN	R3	;R3 = FFF, Negative = TRUE
-	OUT	R0	;OutDevice should contain the same values as InDevice
-	OUT	R1
-	OUT	R2
-	OUT	R3
-	STOP

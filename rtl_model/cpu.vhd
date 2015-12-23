e5b17c355a4fa95e8de776a26d24189f1ccbab10
maxcpu <max.ehm@gmx.de>
2015-12-23 14:52:41 +0100

    added controller in cpu

--- rtl_model/cpu.vhd
+++ rtl_model/cpu.vhd
@@ -12,6 +12,7 @@ entity CPU is
 		MEM_W_EN : out bit;
 		IO_EN : out bit;
 		IO_TYPE : out bit;
+		ACTIVE : out bit;
 		DEV_RDY : in bit );
 
 end CPU;
@@ -28,8 +29,8 @@ architecture struct of CPU is
 begin
 
 	DATPATH : 	entity WORK.datapath(mixed)
-			port map(	CLK => CLK, 
-					RST => RST,	
+			    port map(	CLK => CLK, 
+                    RST => RST,	
 					OP => OP_TO_DATPATH,
 					D_IN => DAT_TO_DATPATH,
 					FLAGS => FLAGS_TO_CONTR,
@@ -41,6 +42,31 @@ begin
 					D_OUT_2 => DAT_2_TO_CONTR,
 					FC_SEL => FC_SEL_TO_DATPATH,
 					REG_EN => REG_EN_TO_DATPATH );
+					
+    CONTR:  entity WORK.controller(structural)
+            port map(   CLK => CLK,
+                        RST => RST,
+                        D_IN => DATA_IN,
+                        A_IN_1 => DAT_1_TO_CONTR,
+                        A_IN_2 => DAT_2_TO_CONTR,
+                        FLAGS => FLAGS_TO_CONTR,
+                        FC_SEL => FC_SEL_TO_DATPATH,
+                        REG_EN => REG_EN_TO_DATPATH,
+                        SEL_IN => SEL_IN_TO_DATPATH,
+                        SEL_OUT_A => SEL_A_TO_DATPATH,
+                        SEL_OUT_B => SEL_B_TO_DATPATH,
+                        SEL_OUT_C => SEL_C_TO_DATPATH,
+                        OP => OP_TO_DATPATH,
+                        D_OUT => DAT_TO_DATPATH,
+                        DEV_RDY => DEV_RDY,
+                        A_OUT => MEM_ADDR,
+                        W_EN => MEM_W_EN,
+                        D_IN_MUX => SEL_IN,
+                        IO_TYPE => IO_TYPE,
+                        IO_EN => IO_EN,
+                        ACTIVE => ACTIVE );
+                      
+    DATA_OUT <= DAT_1_TO_CONTR;   
 					
 
 end struct;

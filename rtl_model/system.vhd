d367ee2e808efb896e2b870860b17c10a509ad32
maxcpu <max.ehm@gmx.de>
2015-12-28 21:41:34 +0100

    implemented system text io is not working until now

--- rtl_model/system.vhd
+++ rtl_model/system.vhd
@@ -3,9 +3,26 @@ end System;
 
 architecture behavioral of System is
 
+    --clock and reset signals
     signal CLK, RST, ACTIVE : bit;
-    signal MEM_ADDR, D_OUT, D_IN, MEM_OUT, IN_DEV_OUT : bit_vector(11 downto 0);
-    signal IN_DEV_RDY, OUT_DEV_RDY, DEV_RDY, IO_EN, IO_TYPE, SEL_IN, MEM_EN : bit;
+    
+    --cpu data signals
+    signal D_IN, D_OUT : bit_vector(11 downto 0);
+    
+    --cpu control signals
+    signal SEL_IN, IO_EN, IO_TYPE, DEV_RDY : bit;
+    
+    --mem signals
+    signal MEM_ADDR, MEM_OUT : bit_vector(11 downto 0);
+    signal MEM_EN : bit;
+    
+    --in device signals
+    signal IN_DATA_TO_CPU, IN_DATA_FROM_TB : bit_vector(11 downto 0);
+    signal IN_DEV_RDY, IN_REQ, IN_RDY : bit;
+    
+    --out device signals
+    signal OUT_DATA_TO_TB : bit_vector(11 downto 0);
+    signal OUT_DEV_RDY, OUT_REQ, OUT_RDY : bit;
 
 begin
 
@@ -25,7 +42,7 @@ begin
     IN_MUX:     entity WORK.mux12_2x1(RTL)
                 port map(   select_input => SEL_IN,
                             d_in_a => MEM_OUT,
-                            d_in_b => IN_DEV_OUT,
+                            d_in_b => IN_DATA_TO_CPU,
                             d_out => D_IN );
                         
     DEV_MUX:    entity WORK.mux_2x1(RTL)
@@ -35,10 +52,47 @@ begin
                             d_out => DEV_RDY );
                             
     MEM:        entity WORK.Memory(rtl)
-                port map(   clk => CLK,
-                            rst => RST,
+               	port map(   clk => CLK,
                             data_in => D_OUT,
+			    addr => MEM_ADDR,
                             w_en => MEM_EN,
                             data_out => MEM_OUT );
+
+    IN_DEV:     entity WORK.InDevice(behavioral)
+                port map(   CLK => CLK,
+                            RST => RST,
+                            ACC_EN => IO_EN,
+                            ACC_TYPE => IO_TYPE,
+                            DATA_RDY => IN_RDY,
+                            DATA_IN => IN_DATA_FROM_TB,
+                            DEV_RDY => IN_DEV_RDY,
+                            DATA_REQ => IN_REQ,
+                            DATA_OUT => IN_DATA_TO_CPU );
+                            
+    OUT_DEV:    entity WORK.OutDevice(behavioral)
+                port map(   CLK => CLK,
+                            RST => RST,
+                            ACC_EN => IO_EN,
+                            ACC_TYPE => IO_TYPE,
+                            DATA_RDY => OUT_RDY,
+                            DATA_IN => D_OUT,
+                            DEV_RDY => OUT_DEV_RDY,
+                            DATA_REQ => OUT_REQ,
+                            DATA_OUT => OUT_DATA_TO_TB );
+                            
+    IO_TEST:    entity WORK.IO_Testbench(TB)
+                port map(   CLK => CLK,
+                            RST => RST,
+                            IN_REQ => IN_REQ,
+                            IN_READY => IN_RDY,
+                            IN_DATA => IN_DATA_FROM_TB,
+                            OUT_READY => OUT_RDY,
+                            OUT_REQ => OUT_REQ,
+                            OUT_DATA => OUT_DATA_TO_TB );
+                            
+                            
+                            
+
+
                         
 end behavioral;
